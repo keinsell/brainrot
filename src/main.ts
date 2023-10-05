68eb78b545d76c3962fefc1cb7fdb47cd39d450f
⠑⠊⠝⠎⠎⠑⠇ <keinsell@protonmail.com>
2023-10-05 18:35:48 +0200

    .

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,10 @@
-import {bootstrap} from "./index.ts";
+import { bootstrap } from "./index.ts";
 
-const USE_TELEMETRY = true;
+const USE_TELEMETRY =
+  process.env["ENABLE_TELEMETRY"] === "false" ? false : true;
 
 if (USE_TELEMETRY) {
-	await import("./infrastructure/telemetry/open-telemetry.ts")
+  await import("./infrastructure/telemetry/open-telemetry.ts");
 }
 
 await bootstrap();
