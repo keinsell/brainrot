42abe1b2eccb6e4c00468d7d44993597b3126258
⠑⠊⠝⠎⠎⠑⠇ <keinsell@protonmail.com>
2023-10-11 20:47:00 +0200

    .

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,7 @@
+import bytes                 from "bytes"
 import dotenv                from "dotenv";
 import figlet                from "figlet"
+import * as os               from "os"
 import "reflect-metadata";
 import signale               from "signale"
 import {bootstrap}           from "./boostrap.ts";
@@ -9,18 +11,32 @@ import {basicTracerProvider} from "./infrastructure/telemetry/tracers/basic-trac
 
 dotenv.config();
 
+console.log(`Node.js Version: ${process.version}`)
+console.log(`Node.js Platform: ${process.platform}`)
+console.log(`Node.js Architecture: ${process.arch}`)
+console.log(`Node.js Heap: ${bytes(process.memoryUsage().heapUsed)} / ${bytes(process.memoryUsage().heapTotal)}\n`)
+
+console.log(`Environment: ${process.env.NODE_ENV ?? "development"}`)
+console.log(`Host: ${process.env.HOST ?? "localhost"}`)
+console.log(`Port: ${process.env.PORT ?? 3000}`)
+console.log(`Protocol: ${process.env.PROTOCOL ?? "http"}\n`)
+
+console.log(`OS: ${os.userInfo()} ${process.platform} ${process.arch}`)
+const avgCpuSpeedInGhz = (
+	os.cpus().reduce((acc, cpu) => acc + cpu.speed, 0) / os.cpus().length / 1000
+).toFixed(2)
+console.log(`CPU: ${os.cpus()[0].model} @ ${avgCpuSpeedInGhz} Ghz`)
+console.log(`RAM: ${bytes(os.totalmem())} (${bytes(os.freemem())} free)`)
+
 console.log(figlet.textSync("Shabu-server", "Doom"))
 console.log("Shabu is a boilerplate for Nest.js applications with batteries included.\n")
+
 
 // TODO: Add OS Architecture
 // TODO: Add OS Platform
 // TODO: Add OS Release
 // TODO: Add RAM
 // TODO: Add CPU (cores@avgSpeed)
-console.log("System Information:")
-console.log(`\tNode.js Version: ${process.version}`)
-console.log(`\tNode.js Platform: ${process.platform}`)
-console.log(`\tNode.js Architecture: ${process.arch}`)
 
 // TODO: Add check for minimal server requirements
 // RAM > 256MB
