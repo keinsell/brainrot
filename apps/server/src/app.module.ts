853b66eaf629920dec9d576d7367a62a888f30aa
⠑⠊⠝⠎⠎⠑⠇ <keinsell@protonmail.com>
2023-10-17 05:10:10 +0200

    .

--- apps/server/src/app.module.ts
+++ apps/server/src/app.module.ts
@@ -1,10 +1,13 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import {Module}         from '@nestjs/common';
+import {AppController}  from './app.controller.js';
+import {AppService}     from './app.service.js';
+import {DatabaseModule} from "./infrastructure/database/database.module.js"
+
+
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
-})
+	        imports    : [DatabaseModule],
+	        controllers: [AppController],
+	        providers  : [AppService],
+        })
 export class AppModule {}
