1686b8f0674e8cdabc67a7a4b7c6123df1714604
⠑⠊⠝⠎⠎⠑⠇ <keinsell@protonmail.com>
2023-11-02 02:35:48 +0100

    chore: :hammer: add docker-pre-commit and pre-commit-shfmt repos to pre-commit config
    These new repositories have been added to the .pre-commit-config.yaml file to enhance code validation. The "docker-pre-commit" repo helps in checking Docker compose files and "pre-commit-shfmt" assists in shell file formatting. These changes will help maintaining code quality and standards.

--- scripts/00_setup_repository.sh
+++ scripts/00_setup_repository.sh
@@ -1,4 +1,11 @@
+# Use brew to install pre-commit hooks
+# https://pre-commit.com/#install
+brew install pre-commit
 pre-commit install
+
+# Install and update pnpm
+# https://pnpm.io/installation
+npm install -g pnpm
 
 # Install gitleaks to protect against exposing secrets
 # https://github.com/gitleaks/gitleaks
