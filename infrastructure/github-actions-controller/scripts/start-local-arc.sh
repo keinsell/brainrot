e45abfe426ae0b9d165bb90ab3509807649dde2f
Jay <keinsell@protonmail.com>
2023-11-22 19:13:46 +0100

    .

--- infrastructure/github-actions-controller/scripts/start-local-arc.sh
+++ infrastructure/github-actions-controller/scripts/start-local-arc.sh
@@ -28,3 +28,12 @@ helm install "${INSTALLATION_NAME}" \
     --set minRunners=1 \
     --set maxRunners=10 \
     oci://ghcr.io/actions/actions-runner-controller-charts/gha-runner-scale-set
+
+# Upgrade Runner Set
+helm upgrade "${INSTALLATION_NAME}" \
+    --namespace "${NAMESPACE}" \
+    --set githubConfigUrl="${GITHUB_CONFIG_URL}" \
+    --set githubConfigSecret.github_token="${GITHUB_PAT}" \
+    --set minRunners=1 \
+    --set maxRunners=10 \
+    oci://ghcr.io/actions/actions-runner-controller-charts/gha-runner-scale-set
