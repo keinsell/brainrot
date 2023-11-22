4d2d9b7432e29cc06faaec9ecf7e53be3c0b7e7a
Jay <keinsell@protonmail.com>
2023-11-22 20:52:00 +0100

    ci: ☸ add local-based github runner controller (#34)

---
+++ infrastructure/github-actions-controller/README.md
@@ -0,0 +1,1 @@
+# Github Actions Controller (ARC)
---
+++ infrastructure/github-actions-controller/scripts/start-local-arc.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+INSTALLATION_NAME="arc-runner-set"
+NAMESPACE="arc-runners"
+GITHUB_CONFIG_URL="https://github.com/keinsell/methylphenidate"
+
+# TODO: Use Configu
+op read "op://personal/gh-pat-arc-runner/token" > /tmp/gh-pat-arc-runner.token
+
+GITHUB_PAT=$(cat /tmp/gh-pat-arc-runner.token)
+
+# Start Minikube with signle-node configuration
+minikube start -p local-arc
+
+# Install ARC
+helm install arc \
+    --namespace "arc-systems" \
+    --create-namespace \
+    oci://ghcr.io/actions/actions-runner-controller-charts/gha-runner-scale-set-controller
+
+# Deploy Runner Set
+helm install "${INSTALLATION_NAME}" \
+    --namespace "${NAMESPACE}" \
+    --create-namespace \
+    --set githubConfigUrl="${GITHUB_CONFIG_URL}" \
+    --set githubConfigSecret.github_token="${GITHUB_PAT}" \
+    --set minRunners=1 \
+    --set maxRunners=3 \
+    oci://ghcr.io/actions/actions-runner-controller-charts/gha-runner-scale-set
+
+# Upgrade Runner Set
+helm upgrade "${INSTALLATION_NAME}" \
+    --namespace "${NAMESPACE}" \
+    --set githubConfigUrl="${GITHUB_CONFIG_URL}" \
+    --set githubConfigSecret.github_token="${GITHUB_PAT}" \
+    --set minRunners=1 \
+    --set maxRunners=3 \
+    oci://ghcr.io/actions/actions-runner-controller-charts/gha-runner-scale-set
