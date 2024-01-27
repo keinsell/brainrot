0caa1070217f47d9e0d3784ea83acee16a640e0d
Jakub Olan <keinsell@protonmail.com>
2024-01-27 18:07:54 +0100

    feat: ✨ add infisical-managed secrets (#114)

--- infrastructure/environments/development/provider.tf
+++ infrastructure/environments/development/provider.tf
@@ -7,3 +7,8 @@ provider "neon" {
 
 provider "koyeb" {
 }
+
+provider "infisical" {
+  host          = "https://app.infisical.com" # Only required if using self hosted instance of Infisical, default is https://app.infisical.com
+  service_token = var.infisical_service_token
+}
--- infrastructure/environments/development/variables.tf
+++ infrastructure/environments/development/variables.tf
@@ -11,3 +11,7 @@ variable "neon_token" {
 variable "koyeb_token" {
   description = "Developer Token dedicated to interact with Koyeb Platform.\n https://app.koyeb.com/user/settings/api"
 }
+
+variable "infisical_service_token" {
+    description = "Developer Token dedicated to interact with Infisical Platform.\n https://app.infisical.com/project/65b52b18d18e1b9f52830831/members"
+}
