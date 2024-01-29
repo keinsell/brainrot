1be4ee43ca6082b6a4a9a352c827ac2fdd2d1faf
Jakub Olan <keinsell@protonmail.com>
2024-01-30 00:05:59 +0100

    ci: 🔧 deploy server application with koyeb (#122)

--- apps/server/src/common/modules/documentation/documentation-module.ts
+++ apps/server/src/common/modules/documentation/documentation-module.ts
@@ -1,19 +1,20 @@
 import { Module }            from '@nestjs/common'
 import { ServeStaticModule } from '@nestjs/serve-static'
+import {join}                from 'node:path'
 import process               from 'node:process'
+import path                  from 'path'
+import {fileURLToPath}       from 'url'
 import { CompodocModule }    from './compodoc/compodoc.module.js'
 
-
-
-const documentationObjectPath = `${process.cwd()}/src/common/modules/documentation/swagger/public/api`
-
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 @Module( {
 			  imports     : [
 				 CompodocModule,
 				 ServeStaticModule.forRoot( {
-														rootPath  : documentationObjectPath,
-														serveRoot : '/api',
+									rootPath : join( __dirname, 'swagger','public', "api" ),
+					 									serveRoot: "/api",
 													 } ),
 			  ],
 			  controllers : [],
