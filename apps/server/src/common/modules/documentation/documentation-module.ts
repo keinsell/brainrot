6a7390196d0a9487ec9a2d06978132a167e31ce2
Jay <keinsell@protonmail.com>
2024-01-29 23:05:58 +0100

    .

--- apps/server/src/common/modules/documentation/documentation-module.ts
+++ apps/server/src/common/modules/documentation/documentation-module.ts
@@ -1,18 +1,21 @@
 import { Module }            from '@nestjs/common'
 import { ServeStaticModule } from '@nestjs/serve-static'
 import process               from 'node:process'
+import path                  from 'path'
+import {fileURLToPath}       from 'url'
 import { CompodocModule }    from './compodoc/compodoc.module.js'
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
-
-const documentationObjectPath = `${process.cwd()}/src/common/modules/documentation/swagger/public/api`
-
+const absoluteDir = path.resolve(__dirname);
+const dir = path.join(absoluteDir, 'public');
 
 @Module( {
 			  imports     : [
 				 CompodocModule,
 				 ServeStaticModule.forRoot( {
-														rootPath  : documentationObjectPath,
+														rootPath  : dir,
 														serveRoot : '/api',
 													 } ),
 			  ],
