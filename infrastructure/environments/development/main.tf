7f1ea65f1201c3431f12fe6d5983a1d97fe77654
Jay <keinsell@protonmail.com>
2024-01-31 23:17:20 +0100

    .

--- infrastructure/environments/development/main.tf
+++ infrastructure/environments/development/main.tf
@@ -75,8 +75,6 @@ resource "neon_database" "this" {
 }
 
 
-
-
 #resource "koyeb_secret" "secret_dockerhub_registry_configuration" {
 #  name = var.secret_dockerhub_registry_configuration_name
 #  type = "REGISTRY"
@@ -93,13 +91,13 @@ variable "application_http_port" {
 resource "random_pet" "service_name" {}
 
 resource "infisical_secret" "DATABASE_URI" {
-  name        = "DATABASE_URI"
-  value       = "postgres://${neon_role.db_owner.name}:${neon_role.db_owner
+  name  = "DATABASE_URI"
+  value = "postgres://${neon_role.db_owner.name}:${neon_role.db_owner
   .password}@${neon_project.default.branch.endpoint
   .host}:5432/${neon_database.this.name}"
   env_slug    = "dev"
   folder_path = "/"
-  depends_on = [
+  depends_on  = [
     neon_project.default,
     neon_role.db_owner,
     neon_database.this
@@ -111,7 +109,7 @@ resource "infisical_secret" "DATABASE_HOST" {
   value       = "${neon_project.default.branch.endpoint.host}"
   env_slug    = "dev"
   folder_path = "/"
-  depends_on = [
+  depends_on  = [
     neon_project.default,
     neon_role.db_owner,
   ]
@@ -151,7 +149,7 @@ resource "infisical_secret" "DATABASE_PORT" {
   value       = "5432"
   env_slug    = "dev"
   folder_path = "/"
-  depends_on = [
+  depends_on  = [
     neon_project.default,
     neon_role.db_owner,
   ]
@@ -162,7 +160,7 @@ resource "infisical_secret" "DATABASE_NAME" {
   value       = neon_database.this.name
   env_slug    = "dev"
   folder_path = "/"
-  depends_on = [
+  depends_on  = [
     neon_project.default,
     neon_role.db_owner,
     neon_database.this
@@ -170,13 +168,12 @@ resource "infisical_secret" "DATABASE_NAME" {
 }
 
 
-
 resource "infisical_secret" "DATABASE_USER" {
   name        = "DATABASE_USER"
   value       = neon_role.db_owner.name
   env_slug    = "dev"
   folder_path = "/"
-  depends_on = [
+  depends_on  = [
     neon_project.default,
     neon_role.db_owner,
   ]
@@ -187,17 +184,17 @@ resource "infisical_secret" "DATABASE_PASSWORD" {
   value       = neon_role.db_owner.password
   env_slug    = "dev"
   folder_path = "/"
-  depends_on = [
-        neon_project.default,
-        neon_role.db_owner,
-    ]
+  depends_on  = [
+    neon_project.default,
+    neon_role.db_owner,
+  ]
 }
 
 resource "infisical_secret" "DOCKERHUB_USERNAME" {
   name        = "DOCKERHUB_USERNAME"
   env_slug    = "dev"
   folder_path = "/"
-  value      = var.dockerhub_username
+  value       = var.dockerhub_username
 }
 
 
@@ -224,102 +221,102 @@ resource "infisical_secret" "AWS_ACCESS_KEY" {
 
 # Filter out local zones, which are not currently supported 
 # with managed node groups
-data "aws_availability_zones" "available" {
-  filter {
-    name   = "opt-in-status"
-    values = ["opt-in-not-required"]
-  }
-}
-
-locals {
-  cluster_name = "education-eks-${random_string.suffix.result}"
-}
-
-resource "random_string" "suffix" {
-  length  = 8
-  special = false
-}
-
-module "vpc" {
-  source  = "terraform-aws-modules/vpc/aws"
-  version = "5.0.0"
-
-  name = "education-vpc"
-
-  cidr = "10.0.0.0/16"
-  azs  = slice(data.aws_availability_zones.available.names, 0, 3)
-
-  private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
-  public_subnets  = ["10.0.4.0/24", "10.0.5.0/24", "10.0.6.0/24"]
-
-  enable_nat_gateway   = true
-  single_nat_gateway   = true
-  enable_dns_hostnames = true
-
-  public_subnet_tags = {
-    "kubernetes.io/cluster/${local.cluster_name}" = "shared"
-    "kubernetes.io/role/elb"                      = 1
-  }
-
-  private_subnet_tags = {
-    "kubernetes.io/cluster/${local.cluster_name}" = "shared"
-    "kubernetes.io/role/internal-elb"             = 1
-  }
-}
-
-module "eks" {
-  source  = "terraform-aws-modules/eks/aws"
-  version = "19.15.3"
-
-  cluster_name    = local.cluster_name
-  cluster_version = "1.27"
-
-  vpc_id                         = module.vpc.vpc_id
-  subnet_ids                     = module.vpc.private_subnets
-  cluster_endpoint_public_access = true
-
-  eks_managed_node_group_defaults = {
-    ami_type = "AL2_x86_64"
-
-  }
-
-  eks_managed_node_groups = {
-    one = {
-      name = "node-group-1"
-
-      instance_types = ["t3.micro"]
-
-      min_size     = 1
-      max_size     = 1
-      desired_size = 1
-    }
-  }
-}
-
-
-# https://aws.amazon.com/blogs/containers/amazon-ebs-csi-driver-is-now-generally-available-in-amazon-eks-add-ons/ 
-data "aws_iam_policy" "ebs_csi_policy" {
-  arn = "arn:aws:iam::aws:policy/service-role/AmazonEBSCSIDriverPolicy"
-}
-
-module "irsa-ebs-csi" {
-  source  = "terraform-aws-modules/iam/aws//modules/iam-assumable-role-with-oidc"
-  version = "4.7.0"
-
-  create_role                   = true
-  role_name                     = "AmazonEKSTFEBSCSIRole-${module.eks.cluster_name}"
-  provider_url                  = module.eks.oidc_provider
-  role_policy_arns              = [data.aws_iam_policy.ebs_csi_policy.arn]
-  oidc_fully_qualified_subjects = ["system:serviceaccount:kube-system:ebs-csi-controller-sa"]
-}
-
-resource "aws_eks_addon" "ebs-csi" {
-  cluster_name             = module.eks.cluster_name
-  addon_name               = "aws-ebs-csi-driver"
-  addon_version            = "v1.20.0-eksbuild.1"
-  service_account_role_arn = module.irsa-ebs-csi.iam_role_arn
-  tags = {
-    "eks_addon" = "ebs-csi"
-    "terraform" = "true"
-  }
-}
+#data "aws_availability_zones" "available" {
+#  filter {
+#    name   = "opt-in-status"
+#    values = ["opt-in-not-required"]
+#  }
+#}
+#
+#locals {
+#  cluster_name = "education-eks-${random_string.suffix.result}"
+#}
+#
+#resource "random_string" "suffix" {
+#  length  = 8
+#  special = false
+#}
+#
+#module "vpc" {
+#  source  = "terraform-aws-modules/vpc/aws"
+#  version = "5.0.0"
+#
+#  name = "education-vpc"
+#
+#  cidr = "10.0.0.0/16"
+#  azs  = slice(data.aws_availability_zones.available.names, 0, 3)
+#
+#  private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+#  public_subnets  = ["10.0.4.0/24", "10.0.5.0/24", "10.0.6.0/24"]
+#
+#  enable_nat_gateway   = true
+#  single_nat_gateway   = true
+#  enable_dns_hostnames = true
+#
+#  public_subnet_tags = {
+#    "kubernetes.io/cluster/${local.cluster_name}" = "shared"
+#    "kubernetes.io/role/elb"                      = 1
+#  }
+#
+#  private_subnet_tags = {
+#    "kubernetes.io/cluster/${local.cluster_name}" = "shared"
+#    "kubernetes.io/role/internal-elb"             = 1
+#  }
+#}
+#
+#module "eks" {
+#  source  = "terraform-aws-modules/eks/aws"
+#  version = "19.15.3"
+#
+#  cluster_name    = local.cluster_name
+#  cluster_version = "1.27"
+#
+#  vpc_id                         = module.vpc.vpc_id
+#  subnet_ids                     = module.vpc.private_subnets
+#  cluster_endpoint_public_access = true
+#
+#  eks_managed_node_group_defaults = {
+#    ami_type = "AL2_x86_64"
+#
+#  }
+#
+#  eks_managed_node_groups = {
+#    one = {
+#      name = "node-group-1"
+#
+#      instance_types = ["t3.micro"]
+#
+#      min_size     = 1
+#      max_size     = 1
+#      desired_size = 1
+#    }
+#  }
+#}
+#
+#
+## https://aws.amazon.com/blogs/containers/amazon-ebs-csi-driver-is-now-generally-available-in-amazon-eks-add-ons/
+#data "aws_iam_policy" "ebs_csi_policy" {
+#  arn = "arn:aws:iam::aws:policy/service-role/AmazonEBSCSIDriverPolicy"
+#}
+#
+#module "irsa-ebs-csi" {
+#  source  = "terraform-aws-modules/iam/aws//modules/iam-assumable-role-with-oidc"
+#  version = "4.7.0"
+#
+#  create_role                   = true
+#  role_name                     = "AmazonEKSTFEBSCSIRole-${module.eks.cluster_name}"
+#  provider_url                  = module.eks.oidc_provider
+#  role_policy_arns              = [data.aws_iam_policy.ebs_csi_policy.arn]
+#  oidc_fully_qualified_subjects = ["system:serviceaccount:kube-system:ebs-csi-controller-sa"]
+#}
+#
+#resource "aws_eks_addon" "ebs-csi" {
+#  cluster_name             = module.eks.cluster_name
+#  addon_name               = "aws-ebs-csi-driver"
+#  addon_version            = "v1.20.0-eksbuild.1"
+#  service_account_role_arn = module.irsa-ebs-csi.iam_role_arn
+#  tags = {
+#    "eks_addon" = "ebs-csi"
+#    "terraform" = "true"
+#  }
+#}
