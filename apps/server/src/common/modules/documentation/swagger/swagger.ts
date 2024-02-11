69f85d36fa61b620aa8b50ed3f9787a36731045c
Jay <keinsell@protonmail.com>
2024-02-12 00:29:47 +0100

    docs: :sparkles: use `@scalar/nestjs-api-reference`

--- apps/server/src/common/modules/documentation/swagger/swagger.ts
+++ apps/server/src/common/modules/documentation/swagger/swagger.ts
@@ -1,20 +1,12 @@
-import {
-  INestApplication,
-  Logger,
-}                           from '@nestjs/common'
-import {
-  DocumentBuilder,
-  OpenAPIObject,
-  SwaggerModule,
-}                           from '@nestjs/swagger'
-import filedirname          from 'filedirname'
-import fs                   from 'node:fs'
-import path                 from 'path'
-import prettier             from 'prettier'
-import tildify              from 'tildify'
-import {fileURLToPath}      from 'url'
-import { __config }         from '../../../../configs/global/__config.js'
-import { getMetadataStore } from '../../../../utilities/docs-utils/swagger-api-model.js'
+import {INestApplication, Logger}                      from '@nestjs/common'
+import {DocumentBuilder, OpenAPIObject, SwaggerModule} from '@nestjs/swagger'
+import fs                                              from 'node:fs'
+import path                                            from 'path'
+import prettier                                        from 'prettier'
+import tildify                                         from 'tildify'
+import {fileURLToPath}                                 from 'url'
+import {__config}                                      from '../../../../configs/global/__config.js'
+import {getMetadataStore}                              from '../../../../utilities/docs-utils/swagger-api-model.js'
 
 
 
@@ -24,163 +16,144 @@ import { getMetadataStore } from '../../../../utilities/docs-utils/swagger-api-m
  * @param {OpenAPIObject} document - The OpenAPI document to add model functionality to.
  * @return {void}
  */
-function experimentalAddApiModelFunctionality(document : OpenAPIObject) : void
-  {
-	 // ExperimentalL: Force OpenAPI Model Documentation
+function experimentalAddApiModelFunctionality(document: OpenAPIObject): void {
+	// ExperimentalL: Force OpenAPI Model Documentation
 
-	 const modelMetadataStore = getMetadataStore()
+	const modelMetadataStore = getMetadataStore()
 
-	 if ( document.components )
-		{
-		  for ( const definitionKey in document.components.schemas )
-			 {
-				const metatype = modelMetadataStore[ definitionKey ]
+	if (document.components) {
+		for (const definitionKey in document.components.schemas) {
+			const metatype = modelMetadataStore[definitionKey]
 
-				if ( metatype )
-				  {
-					 if ( metatype.name )
-						{
-						  document.components.schemas[ metatype.name ] = document.components.schemas[ definitionKey ]
-						  delete document.components.schemas[ definitionKey ]
-						}
-
-					 if ( metatype.description )
-						{
-						  ( document.components.schemas[ metatype.name ?? definitionKey ] as any ).description
-							 = metatype.description
-						}
-				  }
-			 }
-		}
-
-	 // Experimental
-	 const updateSchema = (schema : any) => {
-		if ( schema?.$ref )
-		  {
-			 const refArray     = schema.$ref.split( '/' )
-			 const originalName = refArray.pop()
-			 const metatype     = modelMetadataStore[ originalName ]
-
-			 if ( metatype?.name )
-				{
-				  refArray.push( metatype.name )
-				  schema.$ref = refArray.join( '/' )
+			if (metatype) {
+				if (metatype.name) {
+					document.components.schemas[metatype.name] = document.components.schemas[definitionKey]
+					delete document.components.schemas[definitionKey]
 				}
-		  }
-	 }
 
-	 // Update Swagger Paths
-	 for ( const pathKey in document.paths )
-		{
-		  for ( const methodKey in document.paths[ pathKey ] )
-			 {
-				// @ts-ignore
-				const operation = document.paths[ pathKey ][ methodKey ]
-
-				if ( operation?.parameters )
-				  {
-					 for ( const param of operation.parameters )
-						{
-						  updateSchema( param.schema ) // references under parameters can be updated
-						}
-				  }
-
-				if ( operation?.requestBody?.content )
-				  {
-					 for ( const mediaTypeKey in operation.requestBody.content )
-						{
-						  const schema = operation.requestBody.content[ mediaTypeKey ].schema
-						  updateSchema( schema ) // references under request bodies can be updated
-						}
-				  }
-
-				if ( operation?.responses )
-				  {
-					 for ( const responseKey in operation.responses )
-						{
-						  const contentType = operation.responses[ responseKey ]?.content
-
-						  for ( const mediaTypeKey in contentType )
-							 {
-								const schema = contentType[ mediaTypeKey ].schema
-								updateSchema( schema ) // references under responses can be updated.
-							 }
-						}
-				  }
-			 }
+				if (metatype.description) {
+					(
+						document.components.schemas[metatype.name ?? definitionKey] as any
+					).description = metatype.description
+				}
+			}
 		}
-  }
+	}
+
+	// Experimental
+	const updateSchema = (schema: any) => {
+		if (schema?.$ref) {
+			const refArray     = schema.$ref.split('/')
+			const originalName = refArray.pop()
+			const metatype     = modelMetadataStore[originalName]
+
+			if (metatype?.name) {
+				refArray.push(metatype.name)
+				schema.$ref = refArray.join('/')
+			}
+		}
+	}
+
+	// Update Swagger Paths
+	for (const pathKey in document.paths) {
+		for (const methodKey in document.paths[pathKey]) {
+			// @ts-ignore
+			const operation = document.paths[pathKey][methodKey]
+
+			if (operation?.parameters) {
+				for (const param of operation.parameters) {
+					updateSchema(param.schema) // references under parameters can be updated
+				}
+			}
+
+			if (operation?.requestBody?.content) {
+				for (const mediaTypeKey in operation.requestBody.content) {
+					const schema = operation.requestBody.content[mediaTypeKey].schema
+					updateSchema(schema) // references under request bodies can be updated
+				}
+			}
+
+			if (operation?.responses) {
+				for (const responseKey in operation.responses) {
+					const contentType = operation.responses[responseKey]?.content
+
+					for (const mediaTypeKey in contentType) {
+						const schema = contentType[mediaTypeKey].schema
+						updateSchema(schema) // references under responses can be updated.
+					}
+				}
+			}
+		}
+	}
+}
 
 
-export async function buildSwaggerDocumentation(app : INestApplication) : Promise<void>
-  {
-	 const logger = new Logger( 'doc:swagger' )
+export async function buildSwaggerDocumentation(app: INestApplication): Promise<OpenAPIObject> {
+	const logger = new Logger('doc:swagger')
 
-	 const swaggerConfig = new DocumentBuilder()
-	 .setTitle( __config.get( 'SERVICE_NAME' ) )
-	 .setDescription( __config.get( 'SERVICE_DESCRIPTION' ) )
-	 .setVersion( '1.0' )
-	 .addTag( 'api' )
-	 .addBearerAuth( {
-							 name         : 'Bearer Token',
-							 type         : 'http',
-							 scheme       : 'bearer',
-							 bearerFormat : 'JWT',
-							 description  : 'JWT Authorization header using the Bearer scheme. Example: "Authorization: Bearer <token>"',
-						  } )
-	 .build()
+	const swaggerConfig = new DocumentBuilder()
+	.setTitle(__config.get('SERVICE_NAME'))
+	.setDescription(__config.get('SERVICE_DESCRIPTION'))
+	.setVersion('1.0')
+	.addTag('api')
+	.addBearerAuth({
+		name:         'Bearer Token',
+		type:         'http',
+		scheme:       'bearer',
+		bearerFormat: 'JWT',
+		description:  'JWT Authorization header using the Bearer scheme. Example: "Authorization: Bearer <token>"',
+	})
+	.build()
 
-	 logger.verbose( `Swagger documentation base built for ${__config.get( 'SERVICE_NAME' )} service.` )
+	logger.verbose(`Swagger documentation base built for ${__config.get('SERVICE_NAME')} service.`)
 
+	const document = SwaggerModule.createDocument(app, swaggerConfig,
 
+		new DocumentBuilder()
+		// https://stackoverflow.com/questions/59376717/global-headers-for-all-controllers-nestjs-swagger
+		// .addGlobalParameters( { in          : 'header', required    :
+		// true, name        : 'x-request-id', description : 'A unique
+		// identifier assigned to the request. Clients can include this
+		// header' + ' to trace and correlate requests across different
+		// services and systems.', schema      : {type : 'string'},
+		// deprecated  : true, example : nanoid( 128 ), } )
+		.build() as any)
 
-	 const document = SwaggerModule.createDocument( app, swaggerConfig,
+	logger.verbose(`Swagger documentation document built for ${__config.get('SERVICE_NAME')} service.`)
 
-																	new DocumentBuilder()
-																	// https://stackoverflow.com/questions/59376717/global-headers-for-all-controllers-nestjs-swagger
-																	// .addGlobalParameters( { in          : 'header', required    :
-																	// true, name        : 'x-request-id', description : 'A unique
-																	// identifier assigned to the request. Clients can include this
-																	// header' + ' to trace and correlate requests across different
-																	// services and systems.', schema      : {type : 'string'},
-																	// deprecated  : true, example : nanoid( 128 ), } )
-																	.build() as any,
-	 )
+	experimentalAddApiModelFunctionality(document)
 
+	//SwaggerModule.setup(ApplicationConfiguration.openapiDocumentationPath, app, document, {
+	//	explorer:  true,
+	//	customCss: new SwaggerTheme('v3').getBuffer("flattop"), // OR newspaper
+	//});
 
-	 logger.verbose( `Swagger documentation document built for ${__config.get( 'SERVICE_NAME' )} service.` )
+	//app.use("/api", apiReference({
+	//	spec: {
+	//		content: document,
+	//	},
+	//}))
 
-	 experimentalAddApiModelFunctionality( document )
+	const __filename = fileURLToPath(import.meta.url);
+	const __dirname  = path.dirname(__filename);
 
-	 //SwaggerModule.setup(ApplicationConfiguration.openapiDocumentationPath, app, document, {
-	 //	explorer:  true,
-	 //	customCss: new SwaggerTheme('v3').getBuffer("flattop"), // OR newspaper
-	 //});
+	const absoluteDir                = path.resolve(__dirname);
+	const documentationDirectoryPath = path.join(absoluteDir, 'public', 'api');
 
-	 //app.use("/api", apiReference({
-	 //	spec: {
-	 //		content: document,
-	 //	},
-	 //}))
+	fs.mkdirSync(documentationDirectoryPath, {recursive: true})
 
-	  const __filename = fileURLToPath(import.meta.url);
-	  const __dirname = path.dirname(__filename);
+	const documentationObjectPath = path.join(documentationDirectoryPath, "openapi3.json");
 
-	  const absoluteDir = path.resolve(__dirname);
-	  const documentationDirectoryPath = path.join(absoluteDir, 'public', 'api');
+	const formattedDocument = await prettier.format(JSON.stringify(document), {
+		parser:   'json-stringify',
+		tabWidth: 2,
+	})
 
-	  fs.mkdirSync(documentationDirectoryPath, { recursive: true })
+	// Save Swagger Documentation to File
+	fs.writeFileSync(documentationObjectPath, formattedDocument)
+	fs.writeFileSync("./openapi3.json", formattedDocument)
 
-	  const documentationObjectPath = path.join(documentationDirectoryPath, "openapi3.json");
+	logger.verbose(`Swagger documentation was snapshot into ${tildify(documentationObjectPath)}`)
 
-	 const formattedDocument = await prettier.format( JSON.stringify( document ), {
-		parser   : 'json-stringify',
-		tabWidth : 2,
-	 } )
-
-	 // Save Swagger Documentation to File
-	 fs.writeFileSync( documentationObjectPath, formattedDocument )
-	  fs.writeFileSync( "./openapi3.json", formattedDocument )
-
-	 logger.verbose( `Swagger documentation was snapshot into ${tildify( documentationObjectPath )}` )
-  }
+	return document
+}
