de062da7a6df0991c5e29f345e260c65b26a231f
Jakub Olan <keinsell@protonmail.com>
2024-02-16 06:00:11 +0100

    docs: 📝 avoid autogen of oas examples (#190)

--- apps/server/src/modules/authentication/value-objects/tokens/refresh-token.ts
+++ apps/server/src/modules/authentication/value-objects/tokens/refresh-token.ts
@@ -1,22 +1,19 @@
-import type { ApiPropertyOptions } from '@nestjs/swagger'
-import jwt                         from 'jsonwebtoken'
-import { JsonWebToken }            from '../../../authtoken/entity/jsonwebtoken.js'
+import type {ApiPropertyOptions} from '@nestjs/swagger'
+import {ApiAccountMockup}        from '../../../../utilities/fixtures/api-account-mockup.js'
+import {JsonWebToken}            from '../../../authtoken/entity/jsonwebtoken.js'
 
 
 
 export class RefreshToken
-  extends JsonWebToken
-  {
+	extends JsonWebToken {
 
 
-	 static API_PROPERTY_OPTIONS : ApiPropertyOptions = {
-		name        : 'refreshToken',
-		description : '...',
-		example     : jwt.sign( {payload : {}}, 'secret' ),
-		examples    : [
-		  jwt.sign( {payload : {}}, 'secret' ),
-		  jwt.sign( {payload : {}}, 'secret' ),
-		  jwt.sign( {payload : {}}, 'secret' ),
+	static API_PROPERTY_OPTIONS: ApiPropertyOptions = {
+		name       : 'refreshToken',
+		description: '...',
+		example    : ApiAccountMockup._examples.jwts[0],
+		examples   : [
+			ApiAccountMockup._examples.jwts[0], ApiAccountMockup._examples.jwts[1], ApiAccountMockup._examples.jwts[2],
 		],
-	 }
-  }
+	}
+}
