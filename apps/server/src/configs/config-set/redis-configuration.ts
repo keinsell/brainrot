f0b0afcb89bebbe473330f82cb6e6214bf11c2df
Jakub Olan <keinsell@protonmail.com>
2024-02-18 13:26:28 +0100

    feat: 🔨 add connection to redis (#199)

---
+++ apps/server/src/configs/config-set/redis-configuration.ts
@@ -0,0 +1,40 @@
+import convict               from 'convict'
+import {IRedisConfiguration} from '../schema/configuration-container.js'
+
+
+
+export const RedisConfiguration: convict.Schema<IRedisConfiguration> = {
+	host    : {
+		doc    : 'The Redis server hostname',
+		format : String,
+		default: 'localhost',
+		env    : 'REDIS_HOST',
+	},
+	port    : {
+		doc    : 'The Redis server port number',
+		format : 'port',
+		default: 6379,
+		env    : 'REDIS_PORT',
+	},
+	username: {
+		doc     : 'Username for the Redis server',
+		format  : String,
+		nullable: true,
+		default : undefined,
+		env     : 'REDIS_USERNAME',
+	},
+	password: {
+		doc     : 'Password for the Redis server',
+		format  : String,
+		default : undefined,
+		nullable: true,
+		env     : 'REDIS_PASSWORD',
+	},
+	url     : {
+		doc     : 'Connection string for the Redis server',
+		format  : String,
+		nullable: true,
+		default : undefined,
+		env     : 'REDIS_URL',
+	},
+}
