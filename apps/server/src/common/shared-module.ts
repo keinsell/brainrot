3061c44e655f4f1f3d8d77cd4b61052ae2b44294
Jay <keinsell@protonmail.com>
2024-02-18 16:18:01 +0100

    🔨 chore: update imports and add ComputeController for testing cache functionality

--- apps/server/src/common/shared-module.ts
+++ apps/server/src/common/shared-module.ts
@@ -9,38 +9,42 @@ import { ObservabilityModule }            from './modules/observability/observab
 
 
 
-@Module( {
-			  imports   : [
-				 ContinuationLocalStorageModule,
-				 AsyncLocalStorageModule,
-				 ConfigModule.forRoot(),
-				 ObservabilityModule,
-				 DatabaseModule,
-				 EventBusModule,
-				 SessionMiddlewareModule.forRoot( {
-																session : {
-																  secret            : 'secretomitted',
-																  rolling           : false,
-																  resave            : false,
-																  saveUninitialized : false,
-																},
-															 } ), // https://stackoverflow.com/questions/56046527/express-session-req-session-touch-not-a-function
-				 //CookieSessionModule.forRoot({
-				 //	session: {
-				 //		name: 'session', secret: 'secretomitted', maxAge: 0
-				 //	}
-				 //})
-			  ],
-			  providers : [],
-			  exports   : [
-				 ObservabilityModule,
-				 DatabaseModule,
-				 ContinuationLocalStorageModule,
-				 AsyncLocalStorageModule,
-				 ConfigModule,
-				 EventBusModule,
-			  ],
-			} )
+@Module({
+	        imports  : [
+		        ContinuationLocalStorageModule,
+		        AsyncLocalStorageModule,
+		        ConfigModule.forRoot(),
+		        ObservabilityModule,
+		        DatabaseModule,
+		        RedisModule,
+		        EventBusModule,
+		        XcacheModule,
+		        SessionMiddlewareModule.forRoot({
+			                                        session: {
+				                                        secret           : 'secretomitted',
+				                                        rolling          : false,
+				                                        resave           : false,
+				                                        saveUninitialized: false,
+			                                        },
+		                                        }), // https://stackoverflow.com/questions/56046527/express-session-req-session-touch-not-a-function
+		        //CookieSessionModule.forRoot({
+		        //	session: {
+		        //		name: 'session', secret: 'secretomitted', maxAge: 0
+		        //	}
+		        //})
+	        ],
+	        providers: [],
+	        exports  : [
+		        ObservabilityModule,
+		        DatabaseModule,
+		        ContinuationLocalStorageModule,
+		        AsyncLocalStorageModule,
+		        ConfigModule,
+		        EventBusModule,
+		        RedisModule,
+		        XcacheModule,
+	        ],
+        })
 export class SharedModule
-  {
-  }
+{
+}
