08d0abc3ba2fc0029e86a8728eef4284497a3d85
Jakub Olan <keinsell@protonmail.com>
2024-02-20 09:45:12 +0100

    fix: 🐛 corrupted `rollup` configuration (#214)

--- experiments/20240218-node-bundlers/rollup.config.mjs
+++ experiments/20240218-node-bundlers/rollup.config.mjs
@@ -1,15 +1,15 @@
 // import autoInstall from '@rollup/plugin-auto-install';
-import beep        from '@rollup/plugin-beep'
-import commonjs    from '@rollup/plugin-commonjs'
+import beep from '@rollup/plugin-beep'
+import commonjs from '@rollup/plugin-commonjs'
 // import typescript from "@rollup/plugin-typescript";
-import json        from '@rollup/plugin-json'
-import resolve     from '@rollup/plugin-node-resolve'
-import typescript  from '@rollup/plugin-typescript'
-import wasm        from '@rollup/plugin-wasm'
+import json from '@rollup/plugin-json'
+import resolve from '@rollup/plugin-node-resolve'
+import typescript from '@rollup/plugin-typescript'
+import wasm from '@rollup/plugin-wasm'
 import nodeNatives from 'rollup-plugin-natives'
 // import fdts from 'rollup-plugin-flat-dts';
 import nodeGlobals from 'rollup-plugin-node-globals'
-import swc         from 'rollup-plugin-swc3'
+import swc from 'rollup-plugin-swc3'
 
 // When building production-ready configuration of Rollup
 // Official repository would be good reference
@@ -23,17 +23,17 @@ import swc         from 'rollup-plugin-swc3'
 //	preset                 : 'recommended',
 //}
 
-const USE_SWC = true
+const USE_SWC = false
 
 const transpilers = {
-	tsc: typescript({
-		                outputToFilesystem: false,
-	                }),
-	swc: swc({sourceMaps: true}),
+    tsc: typescript({
+        outputToFilesystem: false,
+    }),
+    swc: swc({sourceMaps: true}),
 }
 
 const typescriptTranspiler = transpilers.tsc
-const swcTranspiler        = transpilers.swc
+const swcTranspiler = transpilers.swc
 
 //const configs: RollupOptions[] = []
 
@@ -49,62 +49,62 @@ const platform = process.env.npm_config_target_platform || process.env.npm_confi
 const arch = process.env.npm_config_target_arch || process.env.npm_config_arch || process.arch
 
 const platformId = (
-	                   arch === 'arm64'
-                   ) ? `${platform}-arm64v8` : `${platform}-${arch}`
+    arch === 'arm64'
+) ? `${platform}-arm64v8` : `${platform}-${arch}`
 
 export default {
-	input    : 'src/index.ts',
-	treeshake: {
-		preset: 'recommended',
-	},
-	output   : {
-		format   : 'es',
-		file     : 'dist/bin.js',
-		sourcemap: true,
-		plugins  : [],
-	}, // TODO: Find a way to bundle snappy
-	external : ['snappy'],
-	plugins  : [
-		USE_SWC ? swcTranspiler : typescriptTranspiler,
-		// autoInstall({manager: 'pnpm'}),
-		commonjs({
-			         esmExternals           : false,
-			         transformMixedEsModules: true,
-			         sourceMap              : true,
-		         }),
-		nodeGlobals(),
-		resolve({
-			        exportConditions         : [
-				        'node-addons',
-				        'node',
-			        ],
-			        extensions               : ['node'],
-			        allowExportsFolderMapping: false,
-			        browser                  : false,
-			        preferBuiltins           : true,
-		        }),
-		json({compact: true}),
-		nodeNatives({
-			            copyTo         : 'dist/lib',
-			            destDir        : './lib',
-			            targetEsm      : true,
-			            originTransform: arch,
-		            }),
-		wasm({targetEnv: 'node'}),
-		//		production & terser({
-		//			                    toplevel       : true,
-		//			                    keep_fnames    : false,
-		//			                    compress       : true,
-		//			                    mangle         : true,
-		//			                    keep_classnames: false,
-		//			                    enclose        : false,
-		//			                    module         : false,
-		//			                    format         : {
-		//				                    wrap_iife: false,
-		//				                    shebang  : true,
-		//			                    },
-		//		                    }),
-		beep(),
-		// fdts()
-	],
+    input: 'src/index.ts',
+    treeshake: {
+        preset: 'recommended',
+    },
+    output: {
+        format: 'es',
+        file: 'dist/bin.js',
+        sourcemap: true,
+        plugins: [],
+    }, // TODO: Find a way to bundle snappy
+    external: ['snappy'],
+    plugins: [
+        USE_SWC ? swcTranspiler : typescriptTranspiler,
+        // autoInstall({manager: 'pnpm'}),
+        commonjs({
+            esmExternals: false,
+            transformMixedEsModules: true,
+            sourceMap: true,
+        }),
+        nodeGlobals(),
+        resolve({
+            exportConditions: [
+                'node-addons',
+                'node',
+            ],
+            extensions: ['node'],
+            allowExportsFolderMapping: false,
+            browser: false,
+            preferBuiltins: true,
+        }),
+        json({compact: true}),
+        nodeNatives({
+            copyTo: 'dist/lib',
+            destDir: './lib',
+            targetEsm: true,
+            originTransform: arch,
+        }),
+        wasm({targetEnv: 'node'}),
+        //		production & terser({
+        //			                    toplevel       : true,
+        //			                    keep_fnames    : false,
+        //			                    compress       : true,
+        //			                    mangle         : true,
+        //			                    keep_classnames: false,
+        //			                    enclose        : false,
+        //			                    module         : false,
+        //			                    format         : {
+        //				                    wrap_iife: false,
+        //				                    shebang  : true,
+        //			                    },
+        //		                    }),
+        beep(),
+        // fdts()
+    ],
 }
