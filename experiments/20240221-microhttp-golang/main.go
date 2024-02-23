74c628392deda4b52804667906180fd0df30f2b9
Jay <keinsell@protonmail.com>
2024-02-23 08:21:23 +0100

    feat: ✨ use `fiber` as framework for golang

--- experiments/20240221-microhttp-golang/main.go
+++ experiments/20240221-microhttp-golang/main.go
@@ -1,17 +1,40 @@
+// 🚀 Fiber is an Express inspired web framework written in Go with 💖
+// 📌 API Documentation: https://docs.gofiber.io
+// 📝 Github Repository: https://github.com/gofiber/fiber
+
 package main
 
 import (
-    "net/http"
+	"fmt"
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/log"
+	"os"
 )
 
-func helloHandler(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Hello, World!"))
+func main() {
+	// Print current process
+	if fiber.IsChild() {
+		fmt.Printf("[%d] Child\n", os.Getppid())
+	} else {
+		fmt.Printf("[%d] Master\n", os.Getppid())
+	}
+
+	// Fiber instance
+	app := fiber.New(fiber.Config{
+		Prefork: true,
+	})
+
+	// Routes
+	app.Get("/", hello)
+
+	// Start server
+	log.Fatal(app.Listen(":1338"))
+
+	// Run the following command to see all processes sharing port 3000:
+	// sudo lsof -i -P -n | grep LISTEN
 }
 
-func main() {
-	http.HandleFunc("/", helloHandler)
-
-	for {
-		go http.ListenAndServe(":1338", nil)
-	}
+// Handler
+func hello(c *fiber.Ctx) error {
+	return c.SendString("Hello, World!")
 }
