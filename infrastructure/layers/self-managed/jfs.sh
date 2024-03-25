e33fdf1582bf9548ed5a68aaf5e5d2f316d7504d
Jakub Olan <keinsell@protonmail.com>
2024-03-25 10:09:29 +0100

    infra: 🧱 update homelab config (#374)

--- infrastructure/layers/self-managed/jfs.sh
+++ infrastructure/layers/self-managed/jfs.sh
@@ -5,3 +5,13 @@
 #sed -i "s|PUSHER_APP_ID=.*|PUSHER_APP_ID=$(openssl rand -hex 32)|g" /data/coolify/source/.env
 #sed -i "s|PUSHER_APP_KEY=.*|PUSHER_APP_KEY=$(openssl rand -hex 32)|g" /data/coolify/source/.env
 #sed -i "s|PUSHER_APP_SECRET=.*|PUSHER_APP_SECRET=$(openssl rand -hex 32)|g" /data/coolify/source/.env
+
+
+
+cd ~/ && juicefs format --storage s3 \
+    --bucket http://localhost:9000/docker \
+    --access-key keinsell \
+    --secret-key keinsell \
+    sqlite3://myjfs.db myjfs
+
+cd ~/ && juicefs mount sqlite3://myjfs.db ~/jfs -d
--- infrastructure/layers/underground/github-actions-controller/setup-local-kubernetes-cluster.sh
+++
@@ -1,5 +0,0 @@
-# Utility script to setup a local kubernetes cluster using simplest possible
-# method out there (minikube).
-
-minikube start
-minikube addons enable metrics-server
